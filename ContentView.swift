64c43bb6bb12f9e9329d03c739422540e7ccf8f9
Grant Baughman <twbaugh2@gmail.com>
2022-02-01 11:51:00 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,36 @@
+
+import Subsonic
+import SwiftUI
+
+struct ContentView: View {
+    let names = ["fat shark", "hello rune", "pig thing" ]
+    let columns = [
+        GridItem(.adaptive(minimum: 250))
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns){
+                    
+                    ForEach(names, id: \.self) { name in
+                        Button {
+                            play(sound: "Boom.mp3")
+                            print("\(name) was tapped")
+                        } label: {
+                            Image(name)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(25)
+                                .padding(.horizontal)
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("Friend face says I...")
+        }
+        
+        .navigationViewStyle(.stack)
+    }
+}
